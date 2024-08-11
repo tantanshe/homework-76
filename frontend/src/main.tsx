7d8cafc2f762835f15ff70400c23d11ea34e85ad
Tatiana Shishenina <tatyana020711@gmail.com>
2024-08-11 15:59:26 +0600

    Reformatted the code, finished the app

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,10 @@
-import { createRoot } from 'react-dom/client'
-import App from './containers/App'
+import {createRoot} from 'react-dom/client';
+import App from './containers/App';
 import {ThemeProvider} from '@mui/material';
-import theme from './theme'
+import theme from './theme';
 
 createRoot(document.getElementById('root')!).render(
   <ThemeProvider theme={theme}>
-    <App />
+    <App/>
   </ThemeProvider>,
-)
+);
